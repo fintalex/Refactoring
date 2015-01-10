7d65933358a3d24ad84f75c55f7b36d86fa15fd2
fintalex <fintalex@mail>
2015-01-10 11:02:00 +0300

    statement()

--- RefactoringFaulera/RefactoringFaulera/OriginalProgramm/MovieRentalCusomer.cs
+++ RefactoringFaulera/RefactoringFaulera/OriginalProgramm/MovieRentalCusomer.cs
@@ -9,9 +9,9 @@ namespace MovieRentalCusomer
     // класс который представляет данные о фильме
     public class Movie
     {
-        public static readonly int CHILDREN = 2;
-        public static readonly int REGULAR = 0;
-        public static readonly int NEW_RELEASE = 1;
+        public const int CHILDREN = 2;
+        public const int REGULAR = 0;
+        public const int NEW_RELEASE = 1;
         private String _title;
         private int _priceCode;
 
@@ -71,7 +71,47 @@ namespace MovieRentalCusomer
         {
             return _name;
         }
+        public string statement()
+        {
+            double totalAmount = 0;
+            int frequentRenterPoints = 0;
+            List<Rental> rentals = new List<Rental> ();
+            string result = "Учет аренды для " + getName() + "\n";
+            foreach (Rental each in rentals)
+            {
+                double thisAmount = 0;
+                // определить сумму для каждой строки
+                switch (each.getMovie().getPriceCode())
+                {
+                    case Movie.REGULAR:
+                        thisAmount += 2;
+                        if (each.getDaysRented()>2) 
+                            thisAmount += (each.getDaysRented() - 2) *1.5;
+                        break;
+                    case Movie.NEW_RELEASE:
+                        thisAmount += each.getDaysRented()*3;
+                        break;
+                    case Movie.CHILDREN:
+                        thisAmount += 1.5;
+                        if (each.getDaysRented()>3)
+                            thisAmount += (each.getDaysRented()-3)*1.5;
+                        break;
+                }
+                 // добавить очки для активного арендатора
+                frequentRenterPoints++;
+                //бонус за аренду новинки на два дня
+                if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented()>1)
+                    frequentRenterPoints ++;
+                // показать результаты для этой аренды
+                result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount.ToString() + "\n";
+                totalAmount += thisAmount;
+            }
 
+            // добавить нижний колонтитул 
+            result += "Сумма задолженности составляет " + totalAmount.ToString() + "\n";
+            result += "Вы заработали " + frequentRenterPoints.ToString() + " oчков за активность";
+            return result;
+        }
     }
 
 }
